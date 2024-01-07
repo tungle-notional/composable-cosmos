d2d5f8cdb3c509e7860d25d77f8eea68a8eee101
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-01-07 01:41:55 +0000

    wtf

--- flake.nix
+++ flake.nix
@@ -65,6 +65,7 @@
               -X github.com/cosmos/cosmos-sdk/version.Name=centauri
               -X github.com/cosmos/cosmos-sdk/version.AppName=${pname}
               -X github.com/cosmos/cosmos-sdk/version.Version=${version}
+              -X github.com/cosmos/cosmos-sdk/version.Commit="${self'.rev or self'.dirtyRev or "dirty"}""
               -X github.com/cometbft/cometbft/version.TMCoreSemVer="v0.37.2"
             '';
           };
